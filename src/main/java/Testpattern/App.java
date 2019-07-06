871b0bb8593dc946b241dabfeaff6208d7f7e52f
canha1997 <52206201+canha1997@users.noreply.github.com>
2019-07-06 09:22:23 +0700

    Version 2.1

--- src/main/java/Testpattern/App.java
+++ src/main/java/Testpattern/App.java
@@ -1,5 +1,9 @@
 package Testpattern;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner=new Scanner(System.in);
+        Pattern p = Pattern.compile("[A-Z]{4}\\d{2}");
+        System.out.println("moi ban nhap choi String");
+        String match= scanner.nextLine().trim();
+        Matcher matcher=p.matcher(match);
+        System.out.println(matcher.matches());
+//        while (!matcher.matches())
+//        {
+//            System.out.println("Moi ban nhap lai");
+//            match= scanner.nextLine().trim();
+//        }
+
     }
 }
